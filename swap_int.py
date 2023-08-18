fd998f13e48a044d447959cd5c9bb2fc697a9c05
Kevin Koech <kevinkoech@proton.me>
2023-08-18 17:59:59 +0300

    swap two integers

---
+++ swap_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main():
+    x = int(input("x: "))
+    y = int(input("y: "))
+    
+    # swaping
+
+    x, y = y, x
+
+    print(f'x is {x}')
+    print(f'y is {y}')
+
+main()
+
